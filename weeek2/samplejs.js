d0ab35228f4e233aaa67a2ee3daa2c4ac499404b
곽마라 <62375220+GwakMirae@users.noreply.github.com>
2020-07-06 23:55:07 +0900

    Add files via upload

---
+++ weeek2/samplejs.js
@@ -0,0 +1,10 @@
+let a;
+let b = document.getElementById();//=>object 덩어리
+let c = document.getElementsByClassName("b");
+//단이형태 ->그냥 쓰면 된다
+//복수형태 -> 배열로 접근해야한다.
+c[0].innerHTML
+c[1].innerHTML
+
+console.log("2");//->콘솔 출력역할, f12 개발자모드에서만 나옴
+alert("1");//경고창
